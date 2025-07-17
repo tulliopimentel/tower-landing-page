2fa33a572551363f642ea0c82ac15613f8656cc2
tulliopimentel <tulliopimentelb@gmail.com>
2025-07-17 20:45:08 -0300

    alterado caminho de config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/tower-landing-page/'
+    ? '/'
     : '/',
   plugins: [
     vue(),
