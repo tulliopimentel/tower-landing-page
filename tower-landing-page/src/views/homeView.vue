8aa269343ca55fade873f04826eecef628400cf9
tulliopimentel <tulliopimentelb@gmail.com>
2025-07-17 10:36:33 -0300

    corrigido menu hamburguer, ele toma toda a tela agora

--- tower-landing-page/src/views/homeView.vue
+++ tower-landing-page/src/views/homeView.vue
@@ -164,7 +164,7 @@ export default defineComponent({
         },
         0: { // Para telas muito pequenas (ex: mobile portrait)
           slidesPerView: 2, // 1 foto inteira + metade da próxima. Isso ajuda a dar a ideia de carrossel.
-          spaceBetween: 2,
+          spaceBetween: 0
           // Se 1.5 ainda estiver quebrando, tente slidesPerView: 1 e veja se o layout fica estável.
           // Com 1.5, o freeMode é muito importante para a fluidez.
         },
