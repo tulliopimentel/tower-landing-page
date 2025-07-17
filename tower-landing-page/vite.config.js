67937ca84ed520fb88b978faefc0cd4e547ef1c7
tulliopimentel <tulliopimentelb@gmail.com>
2025-07-17 19:22:23 -0300

    configurado vite

--- tower-landing-page/vite.config.js
+++ tower-landing-page/vite.config.js
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/tower-landing-page/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
